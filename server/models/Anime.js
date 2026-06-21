71faca6c783f89150404aaf56cf8cf29e3a1aed3
Nithin <nithin21.v@gmail.com>
2026-06-21 21:45:34 +0530

    added malID to Anime models

--- server/models/Anime.js
+++ server/models/Anime.js
@@ -7,6 +7,7 @@ const platformSchema = new mongoose.Schema({
 
 const animeSchema = new mongoose.Schema({
   title:        { type: String, required: true },
+  malId:        { type: Number, default: null },
   coverImage:   { type: String, default: '' },
   status:       { type: String, enum: ['Watching', 'Completed', 'Dropped', 'Plan to Watch', 'On Hold'], required: true },
   format:       { type: String, enum: ['Series', 'Movie', 'Special', 'OVA'], required: true },
