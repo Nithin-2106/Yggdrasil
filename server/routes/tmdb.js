647cfce2072e071f237cc024a51e6c8fcc64e0b2
Nithin <nithin21.v@gmail.com>
2026-06-24 13:54:26 +0530

    still fixing tmdb

--- server/routes/tmdb.js
+++ server/routes/tmdb.js
@@ -1,20 +1,26 @@
 const express = require('express');
-const router  = express.Router();
-const axios   = require('axios');
+const router = express.Router();
+const axios = require('axios');
 
-const TMDB_KEY  = process.env.TMDB_KEY;
+const TMDB_KEY = process.env.TMDB_KEY;
 const TMDB_BASE = 'https://api.themoviedb.org/3';
 
-router.get('/*', async (req, res) => {
+router.use(async (req, res) => {
   try {
-    const path   = req.params[0];
-    const query  = { ...req.query, api_key: TMDB_KEY };
-    const url    = `${TMDB_BASE}/${path}`;
-    const response = await axios.get(url, { params: query });
+    const url = `${TMDB_BASE}${req.path}`;
+
+    const response = await axios.get(url, {
+      params: {
+        ...req.query,
+        api_key: TMDB_KEY,
+      },
+    });
+
     res.json(response.data);
   } catch (err) {
-    const status = err.response?.status || 500;
-    res.status(status).json({ message: err.message });
+    res.status(err.response?.status || 500).json({
+      message: err.response?.data || err.message,
+    });
   }
 });
 
