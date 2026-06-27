7f2cee2616e3ed4204a217d2c382d92c79a2b724
Nithin <nithin21.v@gmail.com>
2026-06-27 22:39:55 +0530

    cleaned some client

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,11 +1,14 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js            from '@eslint/js'
+import globals        from 'globals'
+import reactHooks     from 'eslint-plugin-react-hooks'
+import reactRefresh   from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+// Flat config for ESLint 10 + React 19
 export default defineConfig([
+  // Ignore built output
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -14,7 +17,7 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      globals: globals.browser,
+      globals:       globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
